eddce201333765190c28307057a1421908b12b6d
toricruzmendiola <mtoricruz@gmail.com>
2020-07-13 13:28:23 -0700

    completed base practice problem. worked a bit on stretch and wrote down what i need. will work on after todays mvp is met

---
+++ whiteboard.py
@@ -0,0 +1,46 @@
+# Print out each element of the following array on a separate line:
+
+a = ["Joe", "2", "Ted", "4.98", "14", "Sam", "void *", "42", "float", "pointers", "5006"]
+
+# You may use whatever programming language you'd like.
+
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+# Because I want to print each elem in the list on a separate line;
+# I would loop through list and print out each element.
+
+for elem in a:
+    print(elem)
+# print(a)
+
+# ========================================
+
+# Print out each element of the following array on a separate line, but this time the input array can contain arrays nested to an arbitrarily deep level:
+b = ['Bob', 'Slack', ['reddit', '89', 101, ['alacritty', '(brackets)', 5, 375]], 0, ['{slice, owned}'], 22]
+# For the above input, the expected output is:
+# Bob
+# Slack
+# reddit
+# 89
+# 101
+# alacritty
+# (brackets)
+# 5
+# 375
+# 0
+# {slice, owned}
+# 22
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+# i need to access specific indexes that are lists and loop through them to list them on a separate line
+
+# i can loop through the list, if it's an elem, print elem elif it is a list, loop through list and say if
+# it is an elem, print elem, if it's a list, loop through list and if it's an elem print elem
+
+for elem in b:
+    if elem == str or int:
+        print(elem)
+    elif elem == list:
+        for elem in list:
+            print(elem)
